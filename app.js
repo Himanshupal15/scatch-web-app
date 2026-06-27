cb1d5130961f5289bde8a65e6e9f55485ec568ac
Himanshu Pal <palhimanshu8476@gmail.com>
2026-06-27 11:47:15 +0530

    Server link added

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ app.use('/products',productsRouter);
 app.use("/", indexRouter);
 
 
-
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT);
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
 
 server.on('error', (err) => {
     console.error('Server failed to start:', err);
